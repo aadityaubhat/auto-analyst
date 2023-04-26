67ceb0ca19c7f0608aaf61971cd855c5b79c54f5
Aaditya Bhat <aadityaubhat@gmail.com>
2023-04-25 21:00:40 -0400

    Added test for prompts

---
+++ tests/unit_tests/prompts/test_prompts.py
@@ -0,0 +1,23 @@
+from auto_analyst import *
+
+
+def test_render_agg_plot_prompt():
+    question = "What is the average rating of all the movies?"
+    prompt = prompts.render_agg_plot_prompt(question)
+    assert (
+        prompt
+        == """
+Determine whether the asked question can be answered using aggregate data or a plot. Answer aggregate or plot.
+Question: How many sales were made in August?
+Type: aggregate
+Question: What is the average amount per transaction?
+Type: aggregate
+Question: Plot the timeseries of ad impressions
+Type: plot
+Question: Who are the top 10 customers by number of transactions?
+Type: aggregate
+Question: Histogram of customer age
+Type: plot
+Question: What is the average rating of all the movies?
+Type:"""
+    )
