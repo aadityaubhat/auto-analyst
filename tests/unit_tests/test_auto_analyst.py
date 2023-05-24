20ff0c64dfceaf087216c92fb46ec74a49176207
Aaditya Bhat <aadityaubhat@gmail.com>
2023-05-24 00:23:05 -0400

    Fixed test_analyze_query test in test_auto_analyst.py

--- tests/unit_tests/test_auto_analyst.py
+++ tests/unit_tests/test_auto_analyst.py
@@ -1,9 +1,16 @@
+from flask import Flask
 from auto_analyst.auto_analyst import AutoAnalyst
 from auto_analyst.databases.sqlite import SQLLite
 from auto_analyst.data_catalog.sample_datacatalog import SampleDataCatalog
 from auto_analyst.config import OPENAI_API_KEY
 from auto_analyst.llms.openai import OpenAILLM, Model
 import pytest
+
+
+@pytest.fixture(scope="module")
+def app():
+    app = Flask(__name__)
+    yield app
 
 
 @pytest.fixture(scope="module")
@@ -19,23 +26,10 @@ def aa():
 
 
 @pytest.mark.llm
-def test_analyze_query(aa):
-    analysis = aa.analyze("What is the total sales by country?")
-    assert (
-        analysis.query.lower()
-        == """select billingcountry, sum(total) as totalsales\nfrom invoice\ngroup by billingcountry""".lower()
-    )
-
-
-# driver_llm = OpenAILLM(OPENAI_API_KEY, Model.GPT_3_5_TURBO)
-# sample_db = SQLLite()
-# sample_datacatalog = SampleDataCatalog(driver_llm)
-
-# aa = AutoAnalyst(
-#     database=sample_db, datacatalog=sample_datacatalog, driver_llm=driver_llm
-# )
-
-# analysis = aa.analyze("What is the total sales by country?")
-
-# print(analysis.query)
-# print(analysis.result_data)
+def test_analyze_query(aa, app):
+    with app.app_context():
+        analysis = aa.analyze("What is the total sales by country?")
+        assert (
+            analysis.query.lower()
+            == """select billingcountry, sum(total) as totalsales\nfrom invoice\ngroup by billingcountry""".lower()
+        )
