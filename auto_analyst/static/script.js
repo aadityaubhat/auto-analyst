e8a4001d6e5ad050d4200d984797642e6b8d2833
Aaditya Bhat <aadityaubhat@gmail.com>
2023-06-08 23:16:24 -0400

    Improved UI - added copy to clipboard button, auto scrolling

--- auto_analyst/static/script.js
+++ auto_analyst/static/script.js
@@ -1,26 +1,56 @@
-function addToChat(sender, message, isQuery = false) {
+// Initialize clipboard for all current and future copy buttons
+let clipboard = new ClipboardJS('.btn');
+
+clipboard.on('success', function (e) {
+    console.log(e);
+    alert('Copied to clipboard!');
+});
+
+clipboard.on('error', function (e) {
+    console.log(e);
+    alert('Failed to copy.');
+});
+
+function addToChat(sender, message, isQuery = false, data = null) {
     let messageItem = $('<div>');
     messageItem.addClass('message');
     messageItem.addClass(sender.toLowerCase());
 
     let senderElement = $('<strong>').text(sender);
     let senderLine = $('<div>').append(senderElement);
+    senderLine.addClass('d-flex justify-content-between align-items-center');
+
+    let copyButton;
+    if (sender.toLowerCase() === 'autoanalyst' && (isQuery || data)) {
+        // Create the copy to clipboard button and append it to the sender line
+        copyButton = $('<button>');
+        copyButton.addClass('btn btn-small btn-outline-secondary ml-2');  // Add Bootstrap classes
+        copyButton.html('❐');
+        copyButton.attr('data-clipboard-text', isQuery ? message : JSON.stringify(data));
+        senderLine.append(copyButton);
+    }
 
     let messageContent;
 
     if (isQuery) {
         messageContent = $('<pre>').append($('<code>').addClass('sql').text(message));
+        messageContent.addClass('query-content'); // Add the query-content class for query messages
         hljs.highlightBlock(messageContent[0]);
     } else {
         messageContent = typeof message === 'string' ? $('<div>').text(message) : message;
-    } 
+    }
+
 
     messageItem.append(senderLine);
     messageItem.append(messageContent);
 
     $('#chatWindow').append(messageItem);
-}
+    scrollToBottom();
 
+    if (copyButton) {
+        new ClipboardJS(copyButton[0]);
+    }
+}
 
 
 function sendMessage() {
@@ -42,46 +72,44 @@ function sendMessage() {
         contentType: 'application/json',
         dataType: 'json',
         headers: { "X-CSRFToken": csrf_token },
-        success: function(response) {
+        success: function (response) {
             let result = response;
             console.log(result);
             loadingMessage.remove(); // Remove loading animation
             if (result.analysis_type === "data") {
                 // Get column names dynamically from result_data
                 let colNames = Object.keys(result.result_data[0]);
-                
+
                 // Create colModel dynamically
-                let colModel = colNames.map(function(name) {
+                let colModel = colNames.map(function (name) {
                     return {
                         name: name,
                         index: name,
                         width: 100
                     };
                 });
-            
+
                 // Create new div element to host the jqGrid
                 let gridContainer = $('<div>');
                 let gridElement = $('<table id="myGrid"></table>');
                 let pagerElement = $('<div id="pager"></div>');
                 gridContainer.append(gridElement);
                 gridContainer.append(pagerElement);
-            
+
                 gridElement.jqGrid({
                     datatype: "local",
                     data: result.result_data,
                     colNames: colNames,
                     colModel: colModel,
-                    rowNum:10,
-                    rowList:[10,20,30],
+                    rowNum: 10,
+                    rowList: [10, 20, 30],
                     pager: pagerElement,
-                    // sortname: colNames[0],  // use the first column name as the sortname
                     viewrecords: true,
-                    sortorder: "desc",
-                    caption:"Analysis Results"
+                    caption: "Analysis Results"
                 });
 
                 // Add gridContainer as a new message to the chat
-                addToChat('AutoAnalyst', gridContainer);
+                addToChat('AutoAnalyst', gridContainer, false, result.result_data);
             }
             else if (result.analysis_type === "plot") {
                 let plotContainer = $('<div>');
@@ -91,17 +119,21 @@ function sendMessage() {
                 // Add plotContainer as a new message to the chat
                 addToChat('AutoAnalyst', plotContainer);
                 console.log(result.result_plot);
-                setTimeout(function() {
-                    let plotData = JSON.parse(result.result_plot);
-                    Plotly.newPlot(plotElement[0], plotData.data, plotData.layout);
-                }, 0);
+
+                if (result.result_plot) {  // Check if result.result_plot is defined
+                    setTimeout(function () {
+                        let plotData = JSON.parse(result.result_plot);
+                        Plotly.newPlot(plotElement[0], plotData.data, plotData.layout);
+                    }, 0);
+                }
             }
+
             else if (result.analysis_type === "query") {
                 addToChat('AutoAnalyst', result.query, true);
             }
-        
+
             $('#error').hide();  // hide the error message on success
-        },        
+        },
         error: function (xhr, status, error) {
             loadingMessage.remove(); // Remove loading animation
             let errorMessage = null;
@@ -121,3 +153,8 @@ $('#messageForm').submit(function (e) {
     e.preventDefault();
     sendMessage();
 });
+
+function scrollToBottom() {
+    let chatWindow = $('#chatWindow');
+    chatWindow.scrollTop(chatWindow.prop("scrollHeight"));
+}
